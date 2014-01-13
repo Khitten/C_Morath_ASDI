ca9661143b3da1f2b219f9fc5542ecde6ca2d1fd
Cassandra <cmorath@fullsail.edu>
2014-01-13 14:48:19 -0500

    Added the working app and startedthe refactoring process
    This is nowhere near working yet.

--- js/main.js
+++ js/main.js
@@ -2,5 +2,52 @@
 //  Cassandra Morath
 //	ASDI 1401
 //  01/06/14
-//GH-Pages Added
+// JavaScript Document
+// JavaScript Document
+$('#home').on('pageinit', function(){
 
+
+ 	$(document).ready (function () {
+    $('div[class^=ui-grid]').children ().each (function () {
+    $(this).html ('<div class="ui-bar ui-bar-b" style="height:60px">' + $(this).html () + '</div>');
+  		});
+	});
+});	
+		
+$('#addItem').on('pageinit', function(){
+
+		var myForm = $('#formId');
+		    myForm.validate({
+			invalidHandler: function(form, validator) {
+			},
+			submitHandler: function() {
+		var data = myForm.serializeArray();
+			storeData(data);
+		}
+	});
+	
+	//any other code needed for addItem page goes here
+	
+});
+
+//The functions below can go inside or outside the pageinit function for the page in which it is needed.
+
+var autofillData = function (){
+	 
+};
+
+var getData = function(){
+
+};
+
+var storeData = function(data){
+	
+}; 
+
+var	deleteItem = function (){
+			
+};
+					
+var clearLocal = function(){
+
+};

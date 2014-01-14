5584dcfc0d85184c369e949ffbdda238171365c5
Cassandra <cmorath@fullsail.edu>
2014-01-13 19:44:01 -0500

    Added some JQUERY

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@
 //	ASDI 1401
 //  01/06/14
 // JavaScript Document
-// JavaScript Document
+
 $('#home').on('pageinit', function(){
 
 
- 	$(document).ready (function () {
+ 	$('#home').on('pageinit', function () {
     $('div[class^=ui-grid]').children ().each (function () {
     $(this).html ('<div class="ui-bar ui-bar-b" style="height:60px">' + $(this).html () + '</div>');
   		});
@@ -15,7 +15,15 @@ $('#home').on('pageinit', function(){
 });	
 		
 $('#addItem').on('pageinit', function(){
-
+	//click events for display, save and clear
+		var diplayLink = $('#display');
+		displayLink.on('click', getData);
+		var save = $('#save');
+		save.on('click', validate);
+		var clear = $('#clear');
+		clear.on('click', clearLocal);
+		
+	//form validate
 		var myForm = $('#formId');
 		    myForm.validate({
 			invalidHandler: function(form, validator) {

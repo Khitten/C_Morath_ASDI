0218698046cb73d3d3a40f6380388836baceee18
Cassandra <cmorath@fullsail.edu>
2014-01-16 14:02:09 -0500

    Beautifier

--- js/main.js
+++ js/main.js
@@ -4,67 +4,65 @@
 //  01/06/14
 // JavaScript Document
 $('#home').on('pageinit', function () {
-	(function ( $ ) {
-    $.fn.FormCache = function( options ) {
-        var settings = $.extend({
-        }, options );
-        
-        function on_change(event) {
-            var input = $(event.target);
-            var key = input.parents('form:first').attr('name');
-            var data = JSON.parse(localStorage[key]);
-            
-            if(input.attr('type') == 'radio' || input.attr('type') == 'checkbox') {
-                data[input.attr('name')] = input.is(':checked');
-            }else {
-                data[input.attr('name')] = input.val();
+    (function ($) {
+        $.fn.FormCache = function (options) {
+            var settings = $.extend({}, options);
+
+            function on_change(event) {
+                var input = $(event.target);
+                var key = input.parents('form:first').attr('name');
+                var data = JSON.parse(localStorage[key]);
+
+                if (input.attr('type') == 'radio' || input.attr('type') == 'checkbox') {
+                    data[input.attr('name')] = input.is(':checked');
+                } else {
+                    data[input.attr('name')] = input.val();
+                }
+
+                localStorage[key] = JSON.stringify(data);
             }
-            
-            localStorage[key] = JSON.stringify(data);
-        }
-        
-        return this.each(function() {    
-            var element = $(this);
-            
-            if(typeof(Storage)!=="undefined"){
-                var key = element.attr('name');
-                
-                var data = false;
-                if(localStorage[key]) {
-                    data = JSON.parse(localStorage[key]);
-                }
-                
-                if(!data) {
-                    localStorage[key] = JSON.stringify({});
-                    data = JSON.parse(localStorage[key]);
-                }
-                element.find('input, select').change(on_change);
-                
-                element.find('input, select').each(function(){
-                    if($(this).attr('type') != 'submit') {
-                        var input = $(this);
-                        var value = data[input.attr('name')];
-                        if(input.attr('type') == 'radio' || input.attr('type') == 'checkbox') {
-                            if(value) {
-                                input.attr('checked', input.is(':checked'));
-                            } else {
-                                input.removeAttr('checked');
-                            }
-                        } else {
-                            input.val(value);
-                        }
+
+            return this.each(function () {
+                var element = $(this);
+
+                if (typeof (Storage) !== "undefined") {
+                    var key = element.attr('name');
+
+                    var data = false;
+                    if (localStorage[key]) {
+                        data = JSON.parse(localStorage[key]);
                     }
-                });
-                
-                
-            }
-            else {
-                alert('local storage is not available');
-            }
-        });
-    };     
-}( jQuery ))
- $('form').FormCache();
+
+                    if (!data) {
+                        localStorage[key] = JSON.stringify({});
+                        data = JSON.parse(localStorage[key]);
+                    }
+                    element.find('input, select').change(on_change);
+
+                    element.find('input, select').each(function () {
+                        if ($(this).attr('type') != 'submit') {
+                            var input = $(this);
+                            var value = data[input.attr('name')];
+                            if (input.attr('type') == 'radio' || input.attr('type') == 'checkbox') {
+                                if (value) {
+                                    input.attr('checked', input.is(':checked'));
+                                } else {
+                                    input.removeAttr('checked');
+                                }
+                            } else {
+                                input.val(value);
+                            }
+                        }
+                    });
+
+
+                } else {
+                    alert('local storage is not available');
+                }
+            });
+        };
+    }(jQuery))
+    $('form').FormCache();
 
 });
 $('#addItem').on('pageinit', function () {
@@ -75,82 +73,80 @@ $('#addItem').on('pageinit', function () {
     save.on('click', validate);
     var clear = $('#clear');
     clear.on('click', clearLocal);
-	
-	//Try this
-	(function ( $ ) {
-    $.fn.FormCache = function( options ) {
-        var settings = $.extend({
-        }, options );
-        
-        function on_change(event) {
-            var input = $(event.target);
-            var key = input.parents('form:first').attr('name');
-            var data = JSON.parse(localStorage[key]);
-            
-            if(input.attr('type') == 'radio' || input.attr('type') == 'checkbox') {
-                data[input.attr('name')] = input.is(':checked');
-            }else {
-                data[input.attr('name')] = input.val();
+
+    //Try this
+    (function ($) {
+        $.fn.FormCache = function (options) {
+            var settings = $.extend({}, options);
+
+            function on_change(event) {
+                var input = $(event.target);
+                var key = input.parents('form:first').attr('name');
+                var data = JSON.parse(localStorage[key]);
+
+                if (input.attr('type') == 'radio' || input.attr('type') == 'checkbox') {
+                    data[input.attr('name')] = input.is(':checked');
+                } else {
+                    data[input.attr('name')] = input.val();
+                }
+
+                localStorage[key] = JSON.stringify(data);
             }
-            
-            localStorage[key] = JSON.stringify(data);
-        }
-        
-        return this.each(function() {    
-            var element = $(this);
-            
-            if(typeof(Storage)!=="undefined"){
-                var key = element.attr('name');
-                
-                var data = false;
-                if(localStorage[key]) {
-                    data = JSON.parse(localStorage[key]);
-                }
-                
-                if(!data) {
-                    localStorage[key] = JSON.stringify({});
-                    data = JSON.parse(localStorage[key]);
-                }
-                element.find('input, select').change(on_change);
-                
-                element.find('input, select').each(function(){
-                    if($(this).attr('type') != 'submit') {
-                        var input = $(this);
-                        var value = data[input.attr('name')];
-                        if(input.attr('type') == 'radio' || input.attr('type') == 'checkbox') {
-                            if(value) {
-                                input.attr('checked', input.is(':checked'));
-                            } else {
-                                input.removeAttr('checked');
-                            }
-                        } else {
-                            input.val(value);
-                        }
+
+            return this.each(function () {
+                var element = $(this);
+
+                if (typeof (Storage) !== "undefined") {
+                    var key = element.attr('name');
+
+                    var data = false;
+                    if (localStorage[key]) {
+                        data = JSON.parse(localStorage[key]);
                     }
-                });
-                
-                
-            }
-            else {
-                alert('local storage is not available');
-            }
-        });
-    };     
-}( jQuery ))
-$('form').FormCache();
+
+                    if (!data) {
+                        localStorage[key] = JSON.stringify({});
+                        data = JSON.parse(localStorage[key]);
+                    }
+                    element.find('input, select').change(on_change);
+
+                    element.find('input, select').each(function () {
+                        if ($(this).attr('type') != 'submit') {
+                            var input = $(this);
+                            var value = data[input.attr('name')];
+                            if (input.attr('type') == 'radio' || input.attr('type') == 'checkbox') {
+                                if (value) {
+                                    input.attr('checked', input.is(':checked'));
+                                } else {
+                                    input.removeAttr('checked');
+                                }
+                            } else {
+                                input.val(value);
+                            }
+                        }
+                    });
+
+
+                } else {
+                    alert('local storage is not available');
+                }
+            });
+        };
+    }(jQuery))
+    $('form').FormCache();
 
     $("#petForm").submit(function (event) {
-		var returnValue = true;
+        var returnValue = true;
         if ($("#phone").val().length === 0) {
             returnValue = false;
-        } 
-		if (returnValue === true) {
-			//serialize and save data
-			var id = Math.floor(Math.random()*100000000001);
-			localStorage.setItem(id, $(this).serializeArray());
-				alert("Pet saved!");
-		}
-		return returnValue;
+        }
+        if (returnValue === true) {
+            //serialize and save data
+            var id = Math.floor(Math.random() * 100000000001);
+            localStorage.setItem(id, $(this).serializeArray());
+            alert("Pet saved!");
+        }
+        return returnValue;
     });
 });
 
@@ -177,7 +173,7 @@ var deleteItem = function () {
 };
 
 var clearLocal = function () {
-	localStorage.clear();
+    localStorage.clear();
 
 };
 

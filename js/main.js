eb001d21beffbba8c795dc466668a67a05edaa37
Cassandra <cmorath@fullsail.edu>
2014-01-16 15:43:01 -0500

    Added comments to validation

--- js/main.js
+++ js/main.js
@@ -3,16 +3,21 @@
 //	ASDI 1401
 //  01/06/14
 // JavaScript Document
+// http://www.developerdrive.com/2013/08/jquery-plugin-for-caching-forms-using-html5-local-storage/
+
+	//Waits for the page to initialize after JQM enhances the page.
 $('#home').on('pageinit', function () {
+	//Saves orm to local storage using JQuery and JSON
+	//Starts the JQuery plug in wrapper, used to maintain chainability
     (function ($) {
         $.fn.FormCache = function (options) {
             var settings = $.extend({}, options);
-
+			//Changes the data object back into a string
             function on_change(event) {
                 var input = $(event.target);
                 var key = input.parents('form:first').attr('name');
                 var data = JSON.parse(localStorage[key]);
-
+				
                 if (input.attr('type') == 'radio' || input.attr('type') == 'checkbox') {
                     data[input.attr('name')] = input.is(':checked');
                 } else {
@@ -24,19 +29,23 @@ $('#home').on('pageinit', function () {
 
             return this.each(function () {
                 var element = $(this);
-
+				//makes sure the broswer allows use of local storage
                 if (typeof (Storage) !== "undefined") {
+					
+					//uses the name attribute to create a key, can also use ID instead
                     var key = element.attr('name');
-
                     var data = false;
                     if (localStorage[key]) {
+						//parse the value
                         data = JSON.parse(localStorage[key]);
                     }
 
                     if (!data) {
+						//stringify
                         localStorage[key] = JSON.stringify({});
                         data = JSON.parse(localStorage[key]);
                     }
+					//Finds out when values are changed
                     element.find('input, select').change(on_change);
 
                     element.find('input, select').each(function () {
@@ -62,7 +71,9 @@ $('#home').on('pageinit', function () {
             });
         };
     }(jQuery))
+	//Call Function
     $('form').FormCache();
+	
 
 });
 $('#addItem').on('pageinit', function () {
@@ -73,67 +84,6 @@ $('#addItem').on('pageinit', function () {
     save.on('click', validate);
     var clear = $('#clear');
     clear.on('click', clearLocal);
-
-    //Try this
-    (function ($) {
-        $.fn.FormCache = function (options) {
-            var settings = $.extend({}, options);
-
-            function on_change(event) {
-                var input = $(event.target);
-                var key = input.parents('form:first').attr('name');
-                var data = JSON.parse(localStorage[key]);
-
-                if (input.attr('type') == 'radio' || input.attr('type') == 'checkbox') {
-                    data[input.attr('name')] = input.is(':checked');
-                } else {
-                    data[input.attr('name')] = input.val();
-                }
-
-                localStorage[key] = JSON.stringify(data);
-            }
-
-            return this.each(function () {
-                var element = $(this);
-
-                if (typeof (Storage) !== "undefined") {
-                    var key = element.attr('name');
-
-                    var data = false;
-                    if (localStorage[key]) {
-                        data = JSON.parse(localStorage[key]);
-                    }
-
-                    if (!data) {
-                        localStorage[key] = JSON.stringify({});
-                        data = JSON.parse(localStorage[key]);
-                    }
-                    element.find('input, select').change(on_change);
-
-                    element.find('input, select').each(function () {
-                        if ($(this).attr('type') != 'submit') {
-                            var input = $(this);
-                            var value = data[input.attr('name')];
-                            if (input.attr('type') == 'radio' || input.attr('type') == 'checkbox') {
-                                if (value) {
-                                    input.attr('checked', input.is(':checked'));
-                                } else {
-                                    input.removeAttr('checked');
-                                }
-                            } else {
-                                input.val(value);
-                            }
-                        }
-                    });
-
-
-                } else {
-                    alert('local storage is not available');
-                }
-            });
-        };
-    }(jQuery))
-    $('form').FormCache();
 
     $("#petForm").submit(function (event) {
         var returnValue = true;

2bea4c7792af03d84a69b813af14a91bfa0c5002
Cassandra <cmorath@fullsail.edu>
2014-01-16 13:52:32 -0500

    Got the Signin and Register button validating and saving to local storage in JSON!! :D

--- js/main.js
+++ js/main.js
@@ -4,6 +4,67 @@
 //  01/06/14
 // JavaScript Document
 $('#home').on('pageinit', function () {
+	(function ( $ ) {
+    $.fn.FormCache = function( options ) {
+        var settings = $.extend({
+        }, options );
+        
+        function on_change(event) {
+            var input = $(event.target);
+            var key = input.parents('form:first').attr('name');
+            var data = JSON.parse(localStorage[key]);
+            
+            if(input.attr('type') == 'radio' || input.attr('type') == 'checkbox') {
+                data[input.attr('name')] = input.is(':checked');
+            }else {
+                data[input.attr('name')] = input.val();
+            }
+            
+            localStorage[key] = JSON.stringify(data);
+        }
+        
+        return this.each(function() {    
+            var element = $(this);
+            
+            if(typeof(Storage)!=="undefined"){
+                var key = element.attr('name');
+                
+                var data = false;
+                if(localStorage[key]) {
+                    data = JSON.parse(localStorage[key]);
+                }
+                
+                if(!data) {
+                    localStorage[key] = JSON.stringify({});
+                    data = JSON.parse(localStorage[key]);
+                }
+                element.find('input, select').change(on_change);
+                
+                element.find('input, select').each(function(){
+                    if($(this).attr('type') != 'submit') {
+                        var input = $(this);
+                        var value = data[input.attr('name')];
+                        if(input.attr('type') == 'radio' || input.attr('type') == 'checkbox') {
+                            if(value) {
+                                input.attr('checked', input.is(':checked'));
+                            } else {
+                                input.removeAttr('checked');
+                            }
+                        } else {
+                            input.val(value);
+                        }
+                    }
+                });
+                
+                
+            }
+            else {
+                alert('local storage is not available');
+            }
+        });
+    };     
+}( jQuery ))
+ $('form').FormCache();
 
 });
 $('#addItem').on('pageinit', function () {
@@ -14,6 +75,69 @@ $('#addItem').on('pageinit', function () {
     save.on('click', validate);
     var clear = $('#clear');
     clear.on('click', clearLocal);
+	
+	//Try this
+	(function ( $ ) {
+    $.fn.FormCache = function( options ) {
+        var settings = $.extend({
+        }, options );
+        
+        function on_change(event) {
+            var input = $(event.target);
+            var key = input.parents('form:first').attr('name');
+            var data = JSON.parse(localStorage[key]);
+            
+            if(input.attr('type') == 'radio' || input.attr('type') == 'checkbox') {
+                data[input.attr('name')] = input.is(':checked');
+            }else {
+                data[input.attr('name')] = input.val();
+            }
+            
+            localStorage[key] = JSON.stringify(data);
+        }
+        
+        return this.each(function() {    
+            var element = $(this);
+            
+            if(typeof(Storage)!=="undefined"){
+                var key = element.attr('name');
+                
+                var data = false;
+                if(localStorage[key]) {
+                    data = JSON.parse(localStorage[key]);
+                }
+                
+                if(!data) {
+                    localStorage[key] = JSON.stringify({});
+                    data = JSON.parse(localStorage[key]);
+                }
+                element.find('input, select').change(on_change);
+                
+                element.find('input, select').each(function(){
+                    if($(this).attr('type') != 'submit') {
+                        var input = $(this);
+                        var value = data[input.attr('name')];
+                        if(input.attr('type') == 'radio' || input.attr('type') == 'checkbox') {
+                            if(value) {
+                                input.attr('checked', input.is(':checked'));
+                            } else {
+                                input.removeAttr('checked');
+                            }
+                        } else {
+                            input.val(value);
+                        }
+                    }
+                });
+                
+                
+            }
+            else {
+                alert('local storage is not available');
+            }
+        });
+    };     
+}( jQuery ))
+$('form').FormCache();
 
     $("#petForm").submit(function (event) {
 		var returnValue = true;
@@ -53,6 +177,7 @@ var deleteItem = function () {
 };
 
 var clearLocal = function () {
+	localStorage.clear();
 
 };
 
